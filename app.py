f38d915e5d0c7d755c8f38419b218693e1bb6e54
jenni4j <jenni4j@gmail.com>
2020-10-06 15:52:33 -0700

    added key metrics

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from dash.dependencies import Input, Output, State
 from pandas_datareader import data as web
 from datetime import datetime as dt
 import yfinance as yf
+import locale
+l = locale.setlocale(locale.LC_ALL, 'en_US.utf-8')
 
 
 app = dash.Dash()
@@ -40,6 +42,20 @@ app.layout = html.Div([
     ], style={'width': '100%', 'display': 'inline-block'})
 ])
 
+# helper functions
+def percentize(number):
+    if type(number) == int or type(number) == float:
+        return "{:.2%}".format(number)
+    else:
+        return number
+
+def multiplize(number):
+    if type(number) == int or type(number) == float:
+        return str(round(number,2)) + 'x'
+    else:
+        return number    
+
+
 # for the graph
 @app.callback(
     Output('my-graph', 'figure'),
@@ -73,12 +89,32 @@ def generate_table(submits,inputted_value):
         summary_dict = yf.Ticker(inputted_value).info
         industry = summary_dict['industry']
         marketCap = summary_dict['marketCap']
+        marketCap = locale.currency(marketCap, grouping=True)
+        trailPE = multiplize(summary_dict['trailingPE'])
+        fwdPE = multiplize(summary_dict['forwardPE'])
+        evToEbitda = multiplize(summary_dict['enterpriseToEbitda'])
+        priceToSales = multiplize(summary_dict['priceToSalesTrailing12Months'])
+        priceToBook = multiplize(summary_dict['priceToBook'])
+        profitMargin = percentize(summary_dict['profitMargins'])
+        revGrowth = percentize(summary_dict['revenueQuarterlyGrowth'])
+        earnGrowth = percentize(summary_dict['earningsQuarterlyGrowth'])
+        divYield = percentize(summary_dict['dividendYield'])
+        payout = percentize(summary_dict['payoutRatio'])
         desc = summary_dict['longBusinessSummary']
         return [html.Tr([html.Td('Industry'),html.Td(industry)])] + \
         [html.Tr([html.Td('Market Cap'),html.Td(marketCap)])] + \
+        [html.Tr([html.Td('Trailing P/E'),html.Td(trailPE)])] + \
+        [html.Tr([html.Td('Forward P/E'),html.Td(fwdPE)])] + \
+        [html.Tr([html.Td('Price/Sales'),html.Td(priceToSales)])] + \
+        [html.Tr([html.Td('Price/Book'),html.Td(priceToBook)])] + \
+        [html.Tr([html.Td('Profit Margin'),html.Td(profitMargin)])] + \
+        [html.Tr([html.Td('Quarterly Revenue Growth'),html.Td(revGrowth)])] + \
+        [html.Tr([html.Td('Quarterly Earnings Growth'),html.Td(earnGrowth)])] + \
+        [html.Tr([html.Td('Dividend Yield'),html.Td(divYield)])] + \
+        [html.Tr([html.Td('Payout Ratio'),html.Td(payout)])] + \
         [html.Tr([html.Td('Company Description'),html.Td(desc)])]   
     else:
-        return [html.Tr(html.Th('Quick Take'))]
+        return [html.Tr(html.Th(''))]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
